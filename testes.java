5b8284750e46a46b0700df683408ee72b36e40aa
Ivan Jardim Junior <jardimjunior@gmail.com>
2022-11-07 12:04:41 -0300

    Update testes.java

--- testes.java
+++ testes.java
@@ -1,1 +1,13 @@
 Teste
+public class testes {
+    public static void main(String[] args) {
+        //LISTA  DE VALORES
+        int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
+        System.out.printf("%s %12s \n", "Index", "Valores");
+        //PERCORRE CADA ELEMENTO E IMPRIME O ÍNDICE COM O VALOR
+        for( int counter = 0; counter < array.length; counter ++){
+            System.out.printf("%5d %4s %4d \n", counter, "=>" , array[ counter ]);
+        }
+    }
+
+}
